4430678d0d972c0231bb87f090e8db2cab3f76b8
rafiqswe <md.rafiqul@nexdecade.com>
2022-09-08 15:19:18 +0600

    User Edit and Read and Create role update for menu item

--- child_menus.sql
+++ child_menus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 07, 2022 at 01:34 PM
+-- Generation Time: Sep 08, 2022 at 11:18 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -34,6 +34,7 @@ CREATE TABLE `child_menus` (
   `menu_method` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `menu_icon` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `menu_id` bigint(20) UNSIGNED NOT NULL,
+  `order_by` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -42,13 +43,13 @@ CREATE TABLE `child_menus` (
 -- Dumping data for table `child_menus`
 --
 
-INSERT INTO `child_menus` (`id`, `name`, `slug`, `menu_method`, `menu_icon`, `menu_id`, `created_at`, `updated_at`) VALUES
-(1, 'Users', 'users', 'get', 'fa-solid:users', 3, '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(2, 'User Create', 'users.create', 'get', 'fa-solid:users', 3, '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(3, 'Roles', 'roles.index', 'get', 'fa-solid:users', 4, '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(4, 'Create Role', 'roles.create', 'get', 'fa-solid:users', 4, '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(5, 'Menus', 'menus.index', 'get', 'ant-design:menu-outlined', 2, '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(6, 'Create Menu', 'menus.create', 'get', 'ant-design:menu-outlined', 2, '2022-09-07 04:08:11', '2022-09-07 04:08:11');
+INSERT INTO `child_menus` (`id`, `name`, `slug`, `menu_method`, `menu_icon`, `menu_id`, `order_by`, `created_at`, `updated_at`) VALUES
+(1, 'Users', 'users', 'get', 'fa-solid:users', 3, NULL, '2022-09-06 22:08:11', '2022-09-06 22:08:11'),
+(2, 'User Create', 'users.create', 'get', 'fa-solid:users', 3, NULL, '2022-09-06 22:08:11', '2022-09-06 22:08:11'),
+(3, 'Roles', 'roles.index', 'get', 'fa-solid:users', 4, NULL, '2022-09-06 22:08:11', '2022-09-06 22:08:11'),
+(4, 'Create Role', 'roles.create', 'get', 'fa-solid:users', 4, NULL, '2022-09-06 22:08:11', '2022-09-06 22:08:11'),
+(5, 'Menus', 'menus.index', 'get', 'ant-design:menu-outlined', 2, NULL, '2022-09-06 22:08:11', '2022-09-06 22:08:11'),
+(6, 'Create Menu', 'menus.create', 'get', 'ant-design:menu-outlined', 2, NULL, '2022-09-06 22:08:11', '2022-09-06 22:08:11');
 
 --
 -- Indexes for dumped tables
--- menus.sql
+++ menus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 07, 2022 at 01:33 PM
+-- Generation Time: Sep 08, 2022 at 11:18 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -33,6 +33,7 @@ CREATE TABLE `menus` (
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `menu_method` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `menu_icon` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `order_by` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -41,14 +42,14 @@ CREATE TABLE `menus` (
 -- Dumping data for table `menus`
 --
 
-INSERT INTO `menus` (`id`, `name`, `slug`, `menu_method`, `menu_icon`, `created_at`, `updated_at`) VALUES
-(1, 'Home', 'dashboard', 'get', 'fa-solid:home', '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(2, 'Menus', '', '', 'ant-design:menu-outlined', '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(3, 'Users', '', '', 'fa-solid:users', '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(4, 'Roles', '', '', 'fa-solid:users', '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(5, 'Orders', '', '', 'fa-solid:shopping-cart', '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(6, 'Settings', '', '', 'ant-design:setting-outlined', '2022-09-07 04:08:11', '2022-09-07 04:08:11'),
-(7, 'Logout', 'logout', 'post', 'bx:log-out-circle', '2022-09-07 04:08:11', '2022-09-07 04:08:11');
+INSERT INTO `menus` (`id`, `name`, `slug`, `menu_method`, `menu_icon`, `order_by`, `created_at`, `updated_at`) VALUES
+(1, 'Home', 'dashboard', 'get', 'fa-solid:home', 1, '2022-09-06 22:08:11', '2022-09-06 22:08:11'),
+(2, 'Menus', '#', NULL, 'ant-design:menu-outlined', 2, '2022-09-06 22:08:11', '2022-09-08 03:10:14'),
+(3, 'Users', '#', NULL, 'fa-solid:users', 3, '2022-09-06 22:08:11', '2022-09-08 03:10:58'),
+(4, 'Roles', '#', NULL, 'fa-solid:users', 4, '2022-09-06 22:08:11', '2022-09-08 03:11:14'),
+(5, 'Orders', '#', NULL, 'fa-solid:shopping-cart', 5, '2022-09-06 22:08:11', '2022-09-08 03:11:29'),
+(6, 'Settings', '#', NULL, 'ant-design:setting-outlined', 6, '2022-09-06 22:08:11', '2022-09-08 03:08:22'),
+(7, 'Logout', 'logout', 'post', 'bx:log-out-circle', 70, '2022-09-06 22:08:11', '2022-09-07 22:36:01');
 
 --
 -- Indexes for dumped tables
@@ -68,7 +69,7 @@ ALTER TABLE `menus`
 -- AUTO_INCREMENT for table `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
